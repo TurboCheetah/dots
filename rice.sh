72e01e6b0f6f7dee670c894bbd6ba8e95aa2e9dc
turbo <me@turbo.ooo>
2021-12-02 02:31:53 -0500

    update rice.sh

--- rice.sh
+++ rice.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-# Dotfiles install script, hosted on https://git.io/JM68O
+# Dotfiles install script, hosted on https://turbo.ooo/rice
+
+# install paru
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# install dependencies
+paru -S --noconfirm bspwm sxhkd picom-ibhagwan-git alacritty cava dunst htop neofetch neovim polybar rofi zsh pywal-git
+
+# install dots
 git clone https://github.com/TurboCheetah/dots.git ~/.dotfiles
 cd ~/.dotfiles
 ./install
