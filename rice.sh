e774e6efa8b095e06f7ba91f7f8b23f6691aced7
turbo <me@turbo.ooo>
2021-12-02 02:46:12 -0500

    update rice.sh

--- rice.sh
+++ rice.sh
@@ -3,12 +3,15 @@
 
 # install paru
 sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
+if ! command -v $HELPER &> /dev/null
+then
+    echo "It seems that you don't have paru installed, I'll install that for you before continuing."
+	git clone https://aur.archlinux.org/paru.git ~/.srcs/paru
+	(cd ~/.srcs/paru/ && makepkg -si )
+fi
 
 # install dependencies
-paru -S --noconfirm bspwm sxhkd picom-ibhagwan-git alacritty cava dunst htop neofetch neovim polybar rofi zsh pywal-git
+paru -S --noconfirm bspwm sxhkd picom-ibhagwan-git alacritty cava dunst htop neofetch neovim polybar rofi zsh python-pywal
 
 # install dots
 git clone https://github.com/TurboCheetah/dots.git ~/.dotfiles
