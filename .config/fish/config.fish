685589a7357d4de9f2b3fe04ecc7f34027614070
Turbo <dev@turbo.ooo>
2022-10-16 10:05:14 -0500

    switch to zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,6 +36,10 @@ set -g hydro_color_duration fae3b0
 # fzf colors
 set -Ux FZF_DEFAULT_OPTS "--color=bg+:#302D41,bg:#1E1E2E,spinner:#F8BD96,hl:#F28FAD --color=fg:#D9E0EE,header:#F28FAD,info:#DDB6F2,pointer:#F8BD96 --color=marker:#F8BD96,fg+:#F2CDCD,prompt:#DDB6F2,hl+:#F28FAD"
 
+
+# zoxide
+zoxide init fish | source
+
 alias v="vim"
 
 # cloudbox aliases
